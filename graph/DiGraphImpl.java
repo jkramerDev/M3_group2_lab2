1df2249ed93c8b2ebf4922ddbd2ac3b1d0daa895
RL328 <113392186+RL328@users.noreply.github.com>
2024-04-23 19:44:47 -0400

    graphs done

--- graph/DiGraphImpl.java
+++ graph/DiGraphImpl.java
@@ -1,7 +1,15 @@
 package graph;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.LinkedList;
 import java.util.List;
+import java.util.Map;
+import java.util.Queue;
+import java.util.Set;
+
+
  
 //test comment another and another
 public class DiGraphImpl implements DiGraph{
@@ -10,44 +18,69 @@ public class DiGraphImpl implements DiGraph{
 
 	@Override
 	public Boolean addNode(GraphNode node) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!nodeList.contains(node)) {
+            nodeList.add(node);
+            return true;
+        }
+		return false;
 	}
 
 	@Override
 	public Boolean removeNode(GraphNode node) {
-		// TODO Auto-generated method stub
-		return null;
+		if (!nodeList.contains(node)) {
+	        return false;
+	    }
+	    nodeList.remove(node);
+	    // Also remove all edges associated with this node
+	    for (GraphNode n : nodeList) {
+	        n.removeNeighbor(node);
+	    }
+	    return true;
 	}
 
 	@Override
 	public Boolean setNodeValue(GraphNode node, String newNodeValue) {
-		// TODO Auto-generated method stub
-		return null;
+		if (nodeList.contains(node)) {
+	        node.setValue(newNodeValue);
+	        return true;
+	    }
+	    return false;
 	}
 
 	@Override
 	public String getNodeValue(GraphNode node) {
-		// TODO Auto-generated method stub
-		return null;
+		return node.getValue();
 	}
 
 	@Override
 	public Boolean addEdge(GraphNode fromNode, GraphNode toNode, Integer weight) {
-		// TODO Auto-generated method stub
-		return null;
+		GraphNode targetFromNode = getNode(fromNode.getValue());
+		GraphNode targetToNode = getNode(toNode.getValue());
+
+		if (targetFromNode == null || targetToNode == null)
+			return false; // nodes don't exist, can't make the edge
+
+		return targetFromNode.addNeighbor(targetToNode, weight);
 	}
 
 	@Override
 	public Boolean removeEdge(GraphNode fromNode, GraphNode toNode) {
-		// TODO Auto-generated method stub
-		return null;
+		if (nodeList.contains(fromNode) && nodeList.contains(toNode)) {
+            return fromNode.removeNeighbor(toNode);
+        }
+        return false;
 	}
 
 	@Override
 	public Boolean setEdgeValue(GraphNode fromNode, GraphNode toNode, Integer newWeight) {
-		// TODO Auto-generated method stub
-		return null;
+		if (fromNode != null && toNode != null) {
+	        Integer existingWeight = fromNode.getDistanceToNeighbor(toNode);
+	        if (existingWeight != null) {
+	            fromNode.addNeighbor(toNode, newWeight);
+	            return true;
+	        }
+	    }
+		return false;
 	}
 
 	@Override
@@ -58,8 +91,8 @@ public class DiGraphImpl implements DiGraph{
 
 	@Override
 	public List<GraphNode> getAdjacentNodes(GraphNode node) {
-		// TODO Auto-generated method stub
-		return null;
+		GraphNode targetNode = getNode(node.getValue());
+		return targetNode.getNeighbors();
 	}
 
 	@Override
@@ -70,38 +103,155 @@ public class DiGraphImpl implements DiGraph{
 
 	@Override
 	public Boolean nodeIsReachable(GraphNode fromNode, GraphNode toNode) {
-		// TODO Auto-generated method stub
-		return null;
+		GraphNode targetFromNode = getNode(fromNode.getValue());
+		GraphNode targetToNode = getNode(toNode.getValue());
+		if (!nodeList.contains(targetFromNode) || !nodeList.contains(targetToNode)) {
+            return false;
+        }
+
+        Set<GraphNode> visited = new HashSet<>();
+        Queue<GraphNode> queue = new LinkedList<>();
+        queue.add(targetFromNode);
+        visited.add(targetFromNode);
+
+        while (!queue.isEmpty()) {
+            GraphNode current = queue.poll();
+            if (current.equals(targetToNode)) {
+                return true;
+            }
+            for (GraphNode neighbor : current.getNeighbors()) {
+                if (!visited.contains(neighbor)) {
+                    visited.add(neighbor);
+                    queue.add(neighbor);
+                }
+            }
+        }
+        return false;
 	}
 
 	@Override
 	public Boolean hasCycles() {
-		// TODO Auto-generated method stub
-		return null;
-	}
+        Set<GraphNode> visited = new HashSet<>();
+        Set<GraphNode> recStack = new HashSet<>();
+
+        for (GraphNode node : nodeList) {
+            if (detectCycle(node, visited, recStack)) {
+                return true;
+            }
+        }
+        return false;
+    }
+
+    private Boolean detectCycle(GraphNode node, Set<GraphNode> visited, Set<GraphNode> recStack) {
+        if (recStack.contains(node)) {
+            return true;  // Cycle found
+        }
+        if (visited.contains(node)) {
+            return false;  // Node already visited, no cycle found along this path
+        }
+
+        visited.add(node);
+        recStack.add(node);
+
+        for (GraphNode neighbor : node.getNeighbors()) {
+            if (detectCycle(neighbor, visited, recStack)) {
+                return true;
+            }
+        }
+
+        recStack.remove(node);
+        return false;
+    }
+
 
 	@Override
 	public List<GraphNode> getNodes() {
-		// TODO Auto-generated method stub
-		return null;
+		return nodeList;
 	}
 
 	@Override
 	public GraphNode getNode(String nodeValue) {
-		// TODO Auto-generated method stub
+		for (GraphNode thisNode : nodeList) {
+			if (thisNode.getValue().equals(nodeValue))
+				return thisNode;
+		}
 		return null;
 	}
 
 	@Override
 	public int fewestHops(GraphNode fromNode, GraphNode toNode) {
-		// TODO Auto-generated method stub
-		return 0;
+		GraphNode fNode = getNode(fromNode.getValue());			//finds the inputed node in the graph
+	    GraphNode tNode = getNode(toNode.getValue());		//finds the inputed node in the graph
+
+	    if (fNode.equals(tNode)) {		//if end is start:
+	        return 0; 		//zero "hops"
+	    }
+	    
+	    Queue<GraphNode> queue = new LinkedList<>();		//initializes queue to store nodes in temporarily 
+	    HashMap<GraphNode, Integer> visited = new HashMap<>();		//initializes HashMap to store nodes along with the number of hops to node
+	    
+	    queue.add(fNode);		//adds the start to the queue
+	    visited.put(fNode, 0); 		//puts the start node in the HashMap along with the number of hops (0)
+	    
+	    while (!queue.isEmpty()) {		//while there are still nodes to be looked at
+	        GraphNode current = queue.poll();		//initializes the current node to be used below
+	        int currentHops = visited.get(current);		//gets the number of hops (if current has already been visited) 
+	        
+	        for (GraphNode neighbor : (ArrayList<GraphNode>) current.getNeighbors()) {		//for all the neighbors of current (brethfirst)
+	            if (!visited.containsKey(neighbor)) {		//if neighbor has not been visited already
+	                visited.put(neighbor, currentHops + 1);		//puts the neighbor along with number of hops into HashMap
+	                queue.add(neighbor);		//adds the neighbor to queue so it's neighbors can be looked at after iteration
+	                if (neighbor.equals(tNode)) {		//if the neighbor is the end node:
+	                    return currentHops + 1; 		//returns number of hops 
+	                }
+	            }
+	        }
+	    }
+	    
+	    return -1;		//no route found "ERROR"
 	}
 
 	@Override
 	public int shortestPath(GraphNode fromNode, GraphNode toNode) {
-		// TODO Auto-generated method stub
-		return 0;
+		GraphNode fNode = getNode(fromNode.getValue());			//finds the inputed node in the graph
+	    GraphNode tNode = getNode(toNode.getValue());		//finds the inputed node in the graph
+		Map<GraphNode, Integer> weights = new HashMap<>();		//initializes map to store the node along with the total weight to that node
+	    Set<GraphNode> visited = new HashSet<>();		//initializes HashSet to store visited nodes
+
+	    for (GraphNode node : nodeList) {		//for all the nodes in the graph:
+	    	weights.put(node, Integer.MAX_VALUE);		//puts nodes and large number into each weight in HashMap
+	    }
+	    weights.put(fNode, 0);		//puts weightp0 0 into starting node in HashMap
+
+	    GraphNode current = fNode;		//initializes current node with starting node
+
+	    while (current != null) {		//while there are still nodes to look at in the graph:
+	        visited.add(current);		//adds the current to the visited Set
+
+	        if (current.equals(tNode)) {		//if the current is the end node
+	            return weights.get(current);		//returns weight to the current
+	        }	
+
+	        for (GraphNode neighbor : current.getNeighbors()) {		//for all the neighbors of the current: 
+	            if (!visited.contains(neighbor)) {		//if neighbor has not been visited: 
+	                int tempWeight = weights.get(current) + current.getDistanceToNeighbor(neighbor);		//stores the weight to the node from the last plus the previous weight
+	                if (tempWeight < weights.get(neighbor)) {		//if less weight :
+	                	weights.put(neighbor, tempWeight);		//puts the temp into the HashMap
+	                }
+	            }
+	        }
+
+	        current = null;		//resets current
+	        int smallestWeight = Integer.MAX_VALUE;		//sets smallest weight to large number
+	        for (GraphNode node : nodeList) {		//for all the nodes in the graph:
+	            if (!visited.contains(node) && weights.get(node) < smallestWeight) {		//if the node is not visited and the weight is less than the smallest weight:
+	            	smallestWeight = weights.get(node);		//sets the smallest weight to node weight
+	                current = node;		//makes current set to node
+	            }
+	        }
+	    }
+
+	    return weights.getOrDefault(tNode, -1);		//no route found "ERROR"
 	}
 	
 	
--- graph/GraphTester.java
+++ graph/GraphTester.java
@@ -1,7 +1,5 @@
 package graph;
 
- 
-
 public class GraphTester {
 
 	private static DiGraph graph;
@@ -11,26 +9,133 @@ public class GraphTester {
 
 		
 		graph = new DiGraphImpl();
+
+		// add nodes
+		TESTaddNode("A");
+		TESTaddNode("B");
+		TESTaddNode("E");
+		TESTaddNode("C");
+		TESTaddNode("D");
+		TESTaddNode("Kramer");
+		TESTaddNode("F");
+		TESTaddNode("G");
+
+
+		// add edges
 		
-		//add nodes
+		TESTaddEdge("A", "B", 1);
+		TESTaddEdge("B", "C", 2);
+		TESTaddEdge("B", "D", 5);
+		TESTaddEdge("C", "D", 1);
+		TESTaddEdge("D", "E", 12);
+		TESTaddEdge("E", "F", 2);
+		TESTaddEdge("F", "G", 5);
+
+		// describe
+		
+		TESTdescribeGraph();
+
+		// test reachablity
+		
+		TESTnodeIsReachable("A", "B");
+		TESTnodeIsReachable("A", "C");
+		TESTnodeIsReachable("A", "D");
+		
+		TESTnodeIsReachable("B", "A");
+		TESTnodeIsReachable("B", "C");
+		TESTnodeIsReachable("B", "D");
+		
+		TESTnodeIsReachable("C", "A");
+		TESTnodeIsReachable("C", "B");
+		TESTnodeIsReachable("C", "D");
+		
+		TESTnodeIsReachable("D", "A");
+		TESTnodeIsReachable("D", "B");
+		TESTnodeIsReachable("D", "C");
+		
+		// test hasCycles
+		
+		TESThasCycles();
+
+		// test fewest hops
+		
+		TESTfewestHops("B", "D");
+		TESTfewestHops("A", "B");
+		TESTfewestHops("C", "G");
+		
+		// test shortest path
+		
+		TESTshortestPath("B", "C");
+		TESTshortestPath("B", "D");
+		TESTshortestPath("A", "G");
+
+	}
+
+
+	private static void TESTfewestHops(String nodeFrom, String nodeTo) {
+		
+		System.out.println("Fewest hops in the graph from  " + nodeFrom + " to " + nodeTo + ": " + graph.fewestHops(new GraphNode(nodeFrom), new GraphNode(nodeTo)));
+		
+	}
+
+	private static void TESTshortestPath(String nodeFrom, String nodeTo) {
+
+		System.out.println("The graph has a shortest path of " + graph.shortestPath(new GraphNode(nodeFrom), new GraphNode(nodeTo)));
 		
 		
-		//add edges
+	}
+	
+	public static void TESThasCycles() {
+
+		System.out.println("The graph has " + (graph.hasCycles() ? " " : "NO") + " cycles");
+
+	}
+
+	public static void TESTaddEdge(String from, String to, Integer weight) {
+
+		say("Attempting to addEdge [" + from + "] to [" + to + "] with weight [" + weight + "]");
+		say(graph.addEdge(new GraphNode(from), new GraphNode(to), weight) ? "SUCCESS" : "FAIL");
+
+	}
+	
+	public static void TESTaddNode(String value) {
+		say("Attempting to addNode [" + value + "]");
+		say(graph.addNode(new GraphNode(value)) ? "SUCCESS" : "FAIL");
+
+	}
+
+	public static void TESTnodeIsReachable(String nodeFrom, String nodeTo) {
+
+		System.out.println("There is " + (graph.nodeIsReachable(new GraphNode(nodeFrom), new GraphNode(nodeTo)) ? "a" : "NO")
+				+ " path from [" + nodeFrom + "] to [" + nodeTo + "]");
+
+	}
+
+	public static void TESTdescribeGraph() {
+
+		for(GraphNode thisNode : graph.getNodes()) {
+			System.out.println("Node [" + thisNode.getValue() + "] ");
+			
+				if(thisNode.getNeighbors().isEmpty()) {
+					System.out.println("has no outgoing connections");
+					
+				}else {
+					System.out.println();
+					for (GraphNode neighbor : thisNode.getNeighbors()) {
+						System.out.println("is connected to");
+					}
+	
+				}
+				
+				System.out.println();
+				
+		}
 		
-		
-		//describe
-		
-		
-		//test reachablity
-		
-		
-		//test hasCycles
-		
-		//test fewest hops
-		
-		//test shortest path
-		
-		
+	}
+
+	public static void say(String s) {
+
+		System.out.println(s);
 		
 	}
 	
